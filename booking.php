58f0b68fd0d6ff3e0460387e81f7adfdbf728e73
Kian Yee <kianyee.lim@hotmail.com>
2022-11-01 14:04:47 +0800

    fix(booking): movie data render

--- booking.php
+++ booking.php
@@ -7,46 +7,48 @@ if ((!$_GET['id'])) {
     echo "<script>alert('You are Not Suppose to come Here Directly');window.location.href='index.php';</script>";
 }
 include "connection.php";
-$movieQuery = "SELECT * FROM movies WHERE movie_id = $id";
-$movieImageById = mysqli_query($con, $movieQuery);
-$row = mysqli_fetch_array($movieImageById);
+$movieQuery = "SELECT * FROM movies WHERE movie_id=$id";
+$result = mysqli_query($con, $movieQuery);
+$row = mysqli_fetch_assoc($result);
+print_R($row);
 ?>
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="style/styles.css">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-    <title>Book <?php echo $row['movieTitle']; ?> Now</title>
-    <link rel="icon" type="image/png" href="img/logo.png">
-    <script src="_.js "></script>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="style/styles.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
+    integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+  <title>Book <?php echo $row['movieTitle']; ?> Now</title>
+  <link rel="icon" type="image/png" href="img/logo.png">
+  <script src="_.js "></script>
 </head>
 
 <body style="background-color: black;">
-    <div class="booking-panel">
-        <div class="booking-panel-section booking-panel-section1">
-            <h1>RESERVE YOUR TICKET</h1>
-        </div>
-        <div class="booking-panel-section booking-panel-section2" onclick="window.history.go(-1); return false;">
-            <i class="fas fa-2x fa-times"></i>
-        </div>
-        <div class="booking-panel-section booking-panel-section3">
-            <div class="movie-box">
-                <?php
-                echo '<img src="' . $row['poster'] . '" alt="">';
+  <div class="booking-panel">
+    <div class="booking-panel-section booking-panel-section1">
+      <h1>RESERVE YOUR TICKET</h1>
+    </div>
+    <div class="booking-panel-section booking-panel-section2" onclick="window.history.go(-1); return false;">
+      <i class="fas fa-2x fa-times"></i>
+    </div>
+    <div class="booking-panel-section booking-panel-section3">
+      <div class="movie-box">
+        <?php
+                echo '<img src="' . $row['image'] . '" alt="">';
                 ?>
-            </div>
-        </div>
-        <div class="booking-panel-section booking-panel-section4">
-            <div class="title"><?php echo $row['title']; ?></div>
-            <div class="movie-information">
-                <table>
-                    <tr>
-                        <td>GENGRE</td>
-                        <td><?php echo $row['genre']; ?></td>
-                    </tr>
-                    <!-- <tr>
+      </div>
+    </div>
+    <div class="booking-panel-section booking-panel-section4">
+      <div class="title"><?php echo $row['title']; ?></div>
+      <div class="movie-information">
+        <table>
+          <tr>
+            <td>GENGRE</td>
+            <td><?php echo $row['genre']; ?></td>
+          </tr>
+          <!-- <tr>
                         <td>DURATION</td>
                         <td><?php echo $row['']; ?></td>
                     </tr>
@@ -62,65 +64,65 @@ $row = mysqli_fetch_array($movieImageById);
                         <td>ACTORS</td>
                         <td><?php echo $row['movieActors']; ?></td>
                     </tr> -->
-                </table>
-            </div>
-            <div class="booking-form-container">
-                <form action="verify.php" method="POST">
+        </table>
+      </div>
+      <div class="booking-form-container">
+        <form action="verify.php" method="POST">
 
 
-                    <select name="theatre" required>
-                        <option value="" disabled selected>THEATRE</option>
-                        <option value="main-hall">Main Hall</option>
-                        <option value="vip-hall">VIP Hall</option>
-                        <option value="private-hall">Private Hall</option>
-                    </select>
+          <select name="theatre" required>
+            <option value="" disabled selected>THEATRE</option>
+            <option value="main-hall">Main Hall</option>
+            <option value="vip-hall">VIP Hall</option>
+            <option value="private-hall">Private Hall</option>
+          </select>
 
-                    <select name="type" required>
-                        <option value="" disabled selected>TYPE</option>
-                        <option value="3d">3D</option>
-                        <option value="2d">2D</option>
-                        <option value="imax">IMAX</option>
-                        <option value="7d">7D</option>
-                    </select>
+          <select name="type" required>
+            <option value="" disabled selected>TYPE</option>
+            <option value="3d">3D</option>
+            <option value="2d">2D</option>
+            <option value="imax">IMAX</option>
+            <option value="7d">7D</option>
+          </select>
 
-                    <select name="date" required>
-                        <option value="" disabled selected>DATE</option>
-                        <option value="12-3">March 12,2019</option>
-                        <option value="13-3">March 13,2019</option>
-                        <option value="14-3">March 14,2019</option>
-                        <option value="15-3">March 15,2019</option>
-                        <option value="16-3">March 16,2019</option>
-                    </select>
+          <select name="date" required>
+            <option value="" disabled selected>DATE</option>
+            <option value="12-3">March 12,2019</option>
+            <option value="13-3">March 13,2019</option>
+            <option value="14-3">March 14,2019</option>
+            <option value="15-3">March 15,2019</option>
+            <option value="16-3">March 16,2019</option>
+          </select>
 
-                    <select name="hour" required>
-                        <option value="" disabled selected>TIME</option>
-                        <option value="09-00">09:00 AM</option>
-                        <option value="12-00">12:00 AM</option>
-                        <option value="15-00">03:00 PM</option>
-                        <option value="18-00">06:00 PM</option>
-                        <option value="21-00">09:00 PM</option>
-                        <option value="24-00">12:00 PM</option>
-                    </select>
+          <select name="hour" required>
+            <option value="" disabled selected>TIME</option>
+            <option value="09-00">09:00 AM</option>
+            <option value="12-00">12:00 AM</option>
+            <option value="15-00">03:00 PM</option>
+            <option value="18-00">06:00 PM</option>
+            <option value="21-00">09:00 PM</option>
+            <option value="24-00">12:00 PM</option>
+          </select>
 
-                    <input placeholder="First Name" type="text" name="fName" required>
+          <input placeholder="First Name" type="text" name="fName" required>
 
-                    <input placeholder="Last Name" type="text" name="lName">
+          <input placeholder="Last Name" type="text" name="lName">
 
-                    <input placeholder="Phone Number" type="text" name="pNumber" required>
-                    <input placeholder="email" type="email" name="email" required>
-                    <input type="hidden" name="movie_id" value="<?php echo $id; ?>">
+          <input placeholder="Phone Number" type="text" name="pNumber" required>
+          <input placeholder="email" type="email" name="email" required>
+          <input type="hidden" name="movie_id" value="<?php echo $id; ?>">
 
 
 
-                    <button type="submit" value="save" name="submit" class="form-btn">Book a seat</button>
+          <button type="submit" value="save" name="submit" class="form-btn">Book a seat</button>
 
-                </form>
-            </div>
-        </div>
+        </form>
+      </div>
     </div>
+  </div>
 
-    <script src="scripts/jquery-3.3.1.min.js "></script>
-    <script src="scripts/script.js "></script>
+  <script src="scripts/jquery-3.3.1.min.js "></script>
+  <script src="scripts/script.js "></script>
 </body>
 
 </html>
